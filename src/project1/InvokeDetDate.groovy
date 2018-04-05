0025eea9d9447ca94dad48bb55a53940bbafddc4
Indrayan123 <indrayan.chakraborty@gmail.com>
2018-04-05 15:09:01 +0530

    Update InvokeDetDate.groovy

--- src/project1/InvokeDetDate.groovy
+++ src/project1/InvokeDetDate.groovy
@@ -1,5 +1,5 @@
 package project1;
-@GrabResolver(name='LocalRepo', root='http://127.0.0.1:8060/')
+@GrabResolver(name='LocalRepo', root='http://admin:welcome1@127.0.0.1:8060/repository/Local')
 
 @Grab(group='sni.jenkins.wso2',module='wso2-import-export-jar',version='1.1')
 
