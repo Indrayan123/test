c0b54f7cc351466f646c8a1d7578a233f06711b4
Indrayan123 <indrayan.chakraborty@gmail.com>
2018-04-16 20:15:03 +0530

    Update InvokeDetDate.groovy

--- src/project1/InvokeDetDate.groovy
+++ src/project1/InvokeDetDate.groovy
@@ -1,6 +1,6 @@
 package project1;
 @GrabResolver(name='internal', root='http://127.0.0.1:8060/repository/internal')
-@Grab('org.jenkins.wso2:import-export-jar:1.6')
+@Grab('org.jenkins.wso2:import-export-jar:1.0')
 import project1.Sample;
 public class InvokeDetDate
 {
